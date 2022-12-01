11240836234f239faeb50bba00354578181661a8
Carlos Moura <carloseduardo.adm26@gmail.com>
2022-12-01 12:11:31 -0800

    Created view helper to display day and time in friendly format.Created view helper to display price based on wheter it is free or not using helper method created in the Event model. Changed application layout. Imported images and css files.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,19 @@
 module EventsHelper
+  # The methods created in this helper are accessible to any Event-related views.
+  # View helper to display the price of an event.
+  def price(event)
+    # #free? is defined in the Event Model, bc whether an event is free or not is a business descision.
+    if event.free?
+      'Free'
+    else
+      number_to_currency(event.price, precision: 0)
+    end
+  end
+
+  # View helper to display the day and time of an event.
+  def day_and_time(event)
+    if event.starts_at.nil? == false
+      event.starts_at.strftime("%B %d at %I:%M %P")
+    end
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+  def free?
+    # same as => self.price == 0
+    price.blank? || price.zero? # checked wether price attribute is has not been set or is 0
+  end
 end
