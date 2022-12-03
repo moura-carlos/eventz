ce943af7c7838fc917d6e36b18cb64f8d81f83f5
Carlos Moura <carloseduardo.adm26@gmail.com>
2022-12-03 14:37:28 -0800

    Added partial to display success message when successfully creating or updating event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,8 +15,9 @@ class EventsController < ApplicationController
   def update
     @event = Event.find(params[:id])
     if @event.update(event_params)
+      # flash[:notice] = 'Event successfully updated!' => same as second part line 20
       # redirect_to @event = redict_to event_path(@event)
-      redirect_to @event
+      redirect_to @event, notice: 'Event successfully updated!'
     else
       render :edit, status: :unprocessable_entity
     end
@@ -30,7 +31,7 @@ class EventsController < ApplicationController
     # @event = Event.create(event_params) => creates new event and saves to database.
     @event = Event.new(event_params)
     if @event.save
-      redirect_to @event
+      redirect_to @event, notice: 'Event successfully created!'
     else
       render :new, status: :unprocessable_entity
     end
