cacd08c3406cb71b0dbba8d51288ce823daa48e6
Carlos Moura <carloseduardo.adm26@gmail.com>
2022-12-05 10:55:01 -0800

    Created #require_sign_in method to require User to be signed in in order to seel all accounts and see specific account [show and index actions]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+  # Make all of these methods in this application controller private.
+  # That way, they won't be shown like other controller actions.
+  # To put it another way, you can't make a route that runs these methods.
+  # They're intended to be called internally or privately
+  # by the controller that inherits them.
+  private
+  # The code bellow makes the #current_user method available to all controllers
+  def current_user
+    # check that a user ID is in the session before calling find.
+    # The line bellow will only run if there's a user ID in the session.
+    User.find(session[:user_id]) if session[:user_id]
+  end
+
+  # in order to make the #current_user method
+  # available to the views
+  # we use the following code
+  helper_method :current_user
+
+  def require_signin
+    unless current_user
+      redirect_to new_session_url, alert: "Please sign in first!"
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   # View helper method that returns the currently signed in user.
   # since we might use the 'current_user' helper method all over the application,
   # we're putting it in the global Application Helper (application_helper.rb).
-  def current_user
-    # check that a user ID is in the session before calling find.
-    # The line bellow will only run if there's a user ID in the session.
-    User.find(session[:user_id]) if session[:user_id]
-  end
+  # def current_user
+  #   # check that a user ID is in the session before calling find.
+  #   # The line bellow will only run if there's a user ID in the session.
+  #   User.find(session[:user_id]) if session[:user_id]
+  # end
 end
